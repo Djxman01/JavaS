fff633ce29639be2232537457e3d1ccb1446b9ba
Djxman01 <maximonieva1000@gmail.com~>
2023-02-18 11:33:24 -0300

    JQuery

--- script.js
+++ script.js
@@ -1,49 +1,153 @@
+let preguntas_aleatorias = true;
+let mostrar_pantalla_juego_términado = true;
+let reiniciar_puntos_al_reiniciar_el_juego = true;
 
-//  Simulador Interactivo
+window.onload = function () {
+  base_preguntas = readText("base-preguntas.json");
+  interprete_bp = JSON.parse(base_preguntas);
+  escogerPreguntaAleatoria();
+};
 
-const array = []
-let amount = 3;
+let pregunta;
+let posibles_respuestas;
+btn_correspondiente = [
+  select_id("btn1"),
+  select_id("btn2"),
+  select_id("btn3"),
+  select_id("btn4")
+];
+let npreguntas = [];
 
-do {
-    let Nombre = prompt("Ingresar nombre");
-    array.push(Nombre());
-    let Edad = prompt("Ingresar Edad");
-    array.push(Edad);
-    let Pais = prompt("Ingresar Pais")
-    array.push(Pais.toLocaleUpperCase());
-    console.log(array);
+let preguntas_hechas = 0;
+let preguntas_correctas = 0;
 
-} while (array.length != amount)
+function escogerPreguntaAleatoria() {
+  let n;
+  if (preguntas_aleatorias) {
+    n = Math.floor(Math.random() * interprete_bp.length);
+  } else {
+    n = 0;
+  }
 
+  while (npreguntas.includes(n)) {
+    n++;
+    if (n >= interprete_bp.length) {
+      n = 0;
+    }
+    if (npreguntas.length == interprete_bp.length) {
+      //Aquí es donde el juego se reinicia
+      if (mostrar_pantalla_juego_términado) {
+        swal.fire({
+          title: "Juego finalizado",
+          text:
+            "Puntuación: " + preguntas_correctas + "/" + (preguntas_hechas - 1),
+          icon: "success"
+        });
+      }
+      if (reiniciar_puntos_al_reiniciar_el_juego) {
+        preguntas_correctas = 0
+        preguntas_hechas = 0
+      }
+      npreguntas = [];
+    }
+  }
+  npreguntas.push(n);
+  preguntas_hechas++;
 
-function EntraData() {
-    let txt = document.getElementById("tempEntrada");
-    let temp = parseInt(txt.value);
-    return temp;
+  escogerPregunta(n);
 }
-function EscriuData(nomZona, text) {
-    document.getElementById(nomZona).innerHTML = text;
-}
-function toFahrenheit() {
-    let temp = EntraData();
-    temp = ((1.8) * temp + 32);
-    EscriuData('Zona1', temp + " ºF"); // por pantalla la temp resultado
-    FraseRelativa(temp);
-}
-function toCelsius() {
-    let temp = EntraData();
-    let temp2 = ((temp - 32) / 1.8);
-    EscriuData('Zona1', temp2 + " ºC");
-    FraseRelativa(temp);
-}
-function FraseRelativa(temperatura) {
-    let cadena = "-";
-    if ((temperatura >= 14) && (temperatura < 32)) cadena = "Temperatura baja";
-    if ((temperatura >= 32) && (temperatura < 68)) cadena = "Temperatura adecuada";
-    if ((temperatura >= 68) && (temperatura <= 96)) cadena = "Temperatura alta";
-    if (cadena == "-") cadena = "Temperatura desconocida";
-    EscriuData('Zona2', cadena);
-}
- 
 
+function escogerPregunta(n) {
+  pregunta = interprete_bp[n];
+  select_id("categoria").innerHTML = pregunta.categoria;
+  select_id("pregunta").innerHTML = pregunta.pregunta;
+  select_id("numero").innerHTML = n;
+  let pc = preguntas_correctas;
+  if (preguntas_hechas > 1) {
+    select_id("puntaje").innerHTML = pc + "/" + (preguntas_hechas - 1);
+  } else {
+    select_id("puntaje").innerHTML = "";
+  }
 
+  style("imagen").objectFit = pregunta.objectFit;
+  desordenarRespuestas(pregunta);
+  if (pregunta.imagen) {
+    select_id("imagen").setAttribute("src", pregunta.imagen);
+    style("imagen").height = "200px";
+    style("imagen").width = "100%";
+  } else {
+    style("imagen").height = "0px";
+    style("imagen").width = "0px";
+    setTimeout(() => {
+      select_id("imagen").setAttribute("src", "");
+    }, 500);
+  }
+}
+
+function desordenarRespuestas(pregunta) {
+  posibles_respuestas = [
+    pregunta.respuesta,
+    pregunta.incorrecta1,
+    pregunta.incorrecta2,
+    pregunta.incorrecta3,
+  ];
+  posibles_respuestas.sort(() => Math.random() - 0.5);
+
+  select_id("btn1").innerHTML = posibles_respuestas[0];
+  select_id("btn2").innerHTML = posibles_respuestas[1];
+  select_id("btn3").innerHTML = posibles_respuestas[2];
+  select_id("btn4").innerHTML = posibles_respuestas[3];
+}
+
+let suspender_botones = false;
+
+function oprimir_btn(i) {
+  if (suspender_botones) {
+    return;
+  }
+  suspender_botones = true;
+  if (posibles_respuestas[i] == pregunta.respuesta) {
+    preguntas_correctas++;
+    btn_correspondiente[i].style.background = "lightgreen";
+  } else {
+    btn_correspondiente[i].style.background = "pink";
+  }
+  for (let j = 0; j < 4; j++) {
+    if (posibles_respuestas[j] == pregunta.respuesta) {
+      btn_correspondiente[j].style.background = "lightgreen";
+      break;
+    }
+  }
+  setTimeout(() => {
+    reiniciar();
+    suspender_botones = false;
+  }, 3000);
+}
+
+// let p = prompt("numero")
+
+function reiniciar() {
+  for (const btn of btn_correspondiente) {
+    btn.style.background = "white";
+  }
+  escogerPreguntaAleatoria();
+}
+
+function select_id(id) {
+  return document.getElementById(id);
+}
+
+function style(id) {
+  return select_id(id).style;
+}
+
+function readText(ruta_local) {
+  var texto = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", ruta_local, false);
+  xmlhttp.send();
+  if (xmlhttp.status == 200) {
+    texto = xmlhttp.responseText;
+  }
+  return texto;
+}
